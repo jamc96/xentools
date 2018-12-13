38a0ecbb52eef5b354dda739fa7ca0d6d1d06a78
jamc96 <jamc.mejia@gmail.com>
2018-12-13 13:34:22 -0600

    base structure

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include xentools
+class xentools(
+    Enum['present','absent'] $package_ensure   = 'present',
+    Optional[String] $package_xenstore_source  = undef,
+    Optional[String] $package_utilities_source = undef,
+) {
+  # validate if is virtual environment
+  if $facts['is_virtual'] {
+    # include classes
+    include ::xentools::install
+  }
+}
---
+++ manifests/install.pp
@@ -0,0 +1,21 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include xentools::install
+class xentools::install inherits xentools {
+    # resource default 
+    Package {
+      ensure   => $xentools::package_ensure,
+      provider =>  'rpm',
+    }
+    # ensure packages required
+    package {
+      'xe-guest-utilities-xenstore':
+        source  => $xentools::package_xenstore_source;
+      'xe-guest-utilities':
+        source  => $xentools::package_utilities_source,
+        require => Package['xe-guest-utilities-xenstore'];
+    }
+}
