1a79daac104940dc80266f8e3601c6f02b1ea501
jamc96 <jamc.mejia@gmail.com>
2018-12-13 14:06:35 -0600

    fixing linter issues on main file

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,9 +5,9 @@
 # @example
 #   include xentools
 class xentools(
-    Enum['present','absent'] $package_ensure   = 'present',
-    Optional[String] $package_xenstore_source  = undef,
-    Optional[String] $package_utilities_source = undef,
+  Enum['present','absent'] $package_ensure   = 'present',
+  Optional[String] $package_utilities_source = undef,
+  Optional[String] $package_xenstore_source  = undef,
 ) {
   # validate if is virtual environment
   if $facts['is_virtual'] {
